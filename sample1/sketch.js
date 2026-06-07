029dcd779441b7602ab3fcf35c0e2a6f9a318653
chaerin04 <chailinlee2@gmail.com>
2026-06-08 00:06:15 +0900

    Add files via upload

---
+++ sample1/sketch.js
@@ -0,0 +1,90 @@
+function setup() {
+  createCanvas(600, 400);
+  background(120, 170, 90);
+  
+  fill(250);
+  noStroke();
+  ellipse(95, 130, 40, 40);
+  
+  fill(255);
+  stroke(190);
+  ellipse(90, 150, 20, 20);
+  stroke(255);
+  
+  fill(130, 200, 255);
+  stroke(205);
+  strokeWeight(3);
+  ellipse(150, 150, 80, 80);
+  
+  fill(220, 70, 70);
+  stroke(255, 0, 0);
+  strokeWeight(1);
+  ellipse(295, 150, 200, 200);
+  
+  fill(30, 150, 250);
+  stroke(0, 0, 255);
+  strokeWeight(1);
+  ellipse(295, 150, 150, 150);
+  
+  fill(0, 255, 250);
+  stroke(80);
+  strokeWeight(1);
+  ellipse(295, 150, 100, 100);
+  
+  fill(50);
+  stroke(0);
+  strokeWeight(1);
+  ellipse(430, 190, 100, 100);
+  
+  fill(255, 0, 0);
+  stroke(255);
+  triangle(470, 180, 430, 250, 550, 250);
+  
+  fill(30, 150, 250);
+  stroke(0, 0, 255);
+  strokeWeight(1);
+  ellipse(130, 270, 80, 80);
+  
+  fill(255, 0, 0);
+  stroke(255);
+  triangle(150, 180, 180, 320, 240, 250);
+  
+  fill(245, 140, 260);
+  noStroke();
+  ellipse(150, 270, 25, 25);
+  
+  fill(245, 245, 245);
+  stroke(150, 120, 180);
+  strokeWeight(2);
+  ellipse(410, 310, 85, 45);
+  
+  fill(255, 150, 250);
+  stroke(45);
+  circle(350, 250, 60);
+  stroke(155);
+  fill(170, 200, 120);
+  quad(290, 240, 240, 290, 290, 340, 340, 290);
+  
+  fill(205, 0, 0);
+  noStroke();
+  triangle(220, 170, 195, 260, 245, 240);
+  
+  fill(150, 220, 200);
+  noStroke();
+  ellipse(480, 140, 25, 25);
+  
+  stroke(120, 90, 170);
+  strokeWeight(2);
+  line(380, 320, 540, 210);
+  
+  fill(120, 100, 140);
+  stroke(80, 70, 100);
+  triangle(330, 260, 310, 310, 370, 300);
+  
+  fill(255, 200, 0);
+  stroke(200, 150, 0);
+  strokeWeight(1);
+  ellipse(250, 70, 40, 40);
+  
+  }
+ 
